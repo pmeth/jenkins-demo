140f0246c9f068f803c66f886fd42ecdc613699d
Peter Meth <pmeth@delvia.com>
2017-03-29 19:58:47 -0400

    adding some datetime stuffs

--- public/index.php
+++ public/index.php
@@ -3,10 +3,12 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $hello = new App\Hello();
+$date = new DateTime('next week');
 
 $view = [
     'greeting' => $hello->hello('peter'),
     'host' => getenv('EC2_HOST'),
+    'date' => $date->format('Y-m-d'),
 ];
 
 ?>
@@ -18,5 +20,6 @@ $view = [
   <body>
     <h1><?= $view['greeting'] ?></h1>
     <div class="host"><?= $view['host'] ?></div>
+    <div class="date"><?= $view['date'] ?></div>
   </body>
 </html>
